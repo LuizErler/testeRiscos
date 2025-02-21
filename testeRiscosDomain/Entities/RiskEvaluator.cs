a9a843f649d9987495f6b295b1f4dd1ca9f498e7
Luiz Erler <luizerler_360@hotmail.com>
2025-02-21 10:44:35 -0300

    ajuste

--- testeRiscosDomain/Entities/RiskEvaluator.cs
+++ testeRiscosDomain/Entities/RiskEvaluator.cs
@@ -1,7 +1,4 @@
 ﻿using TesteRiscosDomain.Interface;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace TesteRiscosDomain.Entities
 {
--- testeRiscosDomain/Interface/IRiskEvaluator.cs
+++ testeRiscosDomain/Interface/IRiskEvaluator.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using TesteRiscosDomain.Entities;
+﻿using TesteRiscosDomain.Entities;
 
 namespace TesteRiscosDomain.Interface
 {
